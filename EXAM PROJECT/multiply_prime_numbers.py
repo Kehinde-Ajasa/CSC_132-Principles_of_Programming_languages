2264c481063dd13239fa16d427fb630abb9c0e53
Kehinde Ajasa <ti664469@gmail.com>
2023-01-05 01:23:38 +0100

    made changes to the way the position of the numbers appears on screen

--- EXAM PROJECT/multiply_prime_numbers.py
+++ EXAM PROJECT/multiply_prime_numbers.py
@@ -4,29 +4,37 @@ program must first check if number entered by user are prime or not"""
 from Primenumbers import is_prime_number
 
 
-ord_dict = {1:'1st', 2:'2nd', 3:'3rd'}
+ordinal_dictionary = {'1': 'st', '2': 'nd', '3': 'rd'}  # Dictionary to help understand the numbers displayed
+
 
 def user_input():
     """Accepting all user's input and performing operation"""
     product = 1
-    
-    #Changing the list initialization from list() to open-close square brackets as this is more effective more pythonic
-    prime_number_list = []
+    prime_number_list = []  # creating a list
     amount_of_numbers = int(input("How many prime numbers do you want to multiply: "))
     
     for i in range(1, amount_of_numbers + 1):
-        #Allowing for capabilities to handle any arbitrary values passed by the user
-        p_number = int(input(f"Enter your {(ord_dict[i] if i in ord_dict.keys() else (str(i)+'th'))} number: "))
-        
+        # Allowing for capabilities to handle any arbitrary values passed by the user
+        character_change = str(i)
+        length_of_char = len(character_change)
+        if length_of_char == 1:
+            if character_change[-1] in ordinal_dictionary:
+                p_number = int(input(f"Enter your {i}{ordinal_dictionary[character_change[-1]]} number: "))
+            else:
+                p_number = int(input(f"Enter your {i}th number: "))
+        else:
+            if character_change[-1] in ordinal_dictionary and character_change[-2] != "1":
+                p_number = int(input(f"Enter your {i}{ordinal_dictionary[character_change[-1]]} number: "))
+            else:
+                p_number = int(input(f"Enter your {i}th number: "))
+
         while (is_prime_number(p_number)) != 1:
             print("The number you entered is not a prime number, Try again")
             p_number = int(input(f"Enter your {i} number: "))
         if is_prime_number(p_number) == 1:
             prime_number_list.append(p_number)
             product *= p_number
-    
-    #Changing our `return` statement to return the `product value` while printing out the report string to the user
-    #This will allow the function to be useable in any other module.
+
     print(f"The Product of the prime numbers {','.join(str(num) for num in prime_number_list[:-1])} and {prime_number_list[-1]} is {product}")
     return product
 
