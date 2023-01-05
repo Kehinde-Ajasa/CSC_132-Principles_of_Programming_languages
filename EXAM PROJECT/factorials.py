af1942ffacd29fafc262da10955e489cdca66617
Kehinde Ajasa <ti664469@gmail.com>
2023-01-05 01:49:45 +0100

    Program that calculates the factorial of any given number

--- EXAM PROJECT/factorials.py
+++ EXAM PROJECT/factorials.py
@@ -5,10 +5,9 @@ It's denoted with '!'
 as in; n! = 1 * 2 * 3 * (n-1) * n
 """
 
-
 # First, we create a Function; called Factorial
 # This is the function that computes the value of the factorial
-# We've also set a parameter 'num' there;
+# We've also set a parameter 'num' there; 
 # num represents the factorial of the number we're looking for.
 
 def factorial(num):
@@ -16,20 +15,19 @@ def factorial(num):
     #  This is because we're computing the value of the factorial from 1*2*....
     # Giving it another value to begin with will return wrong answers
     fact = 1
-
+    
     #  Using a for loop;
     # A loop is a block of code that runs till the condition is false, or till its broken
-    for i in range(1, num + 1):
-        # In this for loop; the value of 'i' begins from 1;
+    for i in range(1, num +1):
+        # In this for loop; the value of 'i' begins from 1; 
         # and it has an increment of 1 as the loop reruns till the condition of the loop is no more satisfied
         fact = fact * i
-
+    
     # the return keyword converts this statement to something like a variable
     # so, anywhere this function is called, it returns this value (fact)
     return fact
 
-
-# outside the function
+#outside the function
 
 # This next line gets the number whose factorial we're looking for : input()
 # Then this value is converted to an integer int()
