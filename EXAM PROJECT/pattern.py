0b9f88039f295163896aa5d2f02841ddb547b364
Kehinde-Ajasa <ti664469@gmail.com>
2023-01-09 23:57:01 +0100

    converted diamond pattern to basic christmas tree

--- EXAM PROJECT/pattern.py
+++ EXAM PROJECT/pattern.py
@@ -1,21 +1,21 @@
 """This program helps in drawing different types of patterns by specifying the inputs and shapes to be drawn"""
 
 
-# This draws the diamond pattern
-
-
 def get_input():
     """Function to help get all user's input"""
-    middle_star = 9#int(input("How many stars do you want the middle of your diamond to contain: "))
+    middle_star = int(input("How big do you want the size of your tree to be: "))
     return middle_star
 
-print(get_input())
 
-
+# christmas Tree
 def top_part(stars):
     """Function to help display the upper part of the star"""
     jumper = 1
+    width = stars + 2
     for i in range(jumper, stars, 1):
-    	print(" * " * i)
+    	print(("*" * i).center(width, ' '))
+
+    for i in range(jumper, stars, 1):
+    	print(("*" * 1).center(width, ' '))
 
 top_part(get_input())
