b508c068c14a30cb078063169478553a140895f5
Kehinde-Ajasa <ti664469@gmail.com>
2023-01-13 00:26:50 +0100

    Created a dynamic user input means

--- EXAM PROJECT/Diamond Pattern.py
+++ EXAM PROJECT/Diamond Pattern.py
@@ -5,7 +5,11 @@
 def get_input():
     """Function to help get all user's input"""
     star = int(input("How big do you want the size of the diamond to be: "))
-    return star
+    character = input("What letter or character do you want your diamond to be in: ")
+    if len(character) > 1:
+        return "Invalid character entered the values of the character can only be 1 letter long"
+    else:
+        return star, character
 
 
 
@@ -14,15 +18,15 @@ def top_part(stars):
     global jumper
     global width
     jumper = 1
-    width = stars * 2
-    for i in range(jumper, stars, 1):
-        print((" *" * i).center(width, "-"))
+    width = stars[0] * 2
+    for i in range(jumper, stars[0], 1):
+        print((f" {stars[1]}" * i).center(width, "-"))
     
 
 def bottom_part(stars):
 	"""Function to help display the bottom part of the Diamond"""
-	for i in range(stars, jumper-1, -1):
-		print((" *" * i).center(width, "-"))
+	for i in range(stars[0], jumper-1, -1):
+		print((f" {stars[1]}" * i).center(width, "-"))
 
 
 top_part(get_input())
