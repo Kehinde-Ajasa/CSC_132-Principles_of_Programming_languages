141f8f3ae91491014a4890c27254310f66ff7402
Kehinde-Ajasa <ti664469@gmail.com>
2023-01-15 15:10:47 +0100

    Updated Web App version

--- Main_Application/models.py
+++ Main_Application/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    Email = models.EmailField(max_length = 30)
+
+    def __str__(self):
+        return self.Email
--- Main_Application/views.py
+++ Main_Application/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from . models import User
 
 # Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        email = request.POST['user_email']
+        user = User.objects.create(Email = email)
+        user.save()
+    return render(request,'Main_Application/index.html')
+
+def admin(request):
+    pass
