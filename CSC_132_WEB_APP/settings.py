b61892c2c808a91d92c2d2132a0aca388bde872e
Kehinde-Ajasa <ti664469@gmail.com>
2023-01-15 19:38:59 +0100

    os

--- CSC_132_WEB_APP/settings.py
+++ CSC_132_WEB_APP/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
