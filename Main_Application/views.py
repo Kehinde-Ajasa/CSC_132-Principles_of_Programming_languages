79f015df35ef11797badf53565bd3521cb8f8a30
Kehinde-Ajasa <ti664469@gmail.com>
2023-01-19 08:49:06 +0100

    added return

--- Main_Application/views.py
+++ Main_Application/views.py
@@ -22,7 +22,7 @@ def sending_email(mail):
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
         smtp.login(email_sender, email_password)
-        smtp.sendmail(email_sender, email_receiver, em.as_string())
+        return smtp.sendmail(email_sender, email_receiver, em.as_string())
 
 def index(request):
     if request.method == 'POST':
